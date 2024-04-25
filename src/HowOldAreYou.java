fd5ac65d332eff5f2bde86d302210637a8fb475a
22fi121 <22fi121@ms.dendai.ac.jp>
2024-04-25 16:57:47 +0900

    変更２

--- src/HowOldAreYou.java
+++ src/HowOldAreYou.java
@@ -12,20 +12,29 @@ public class HowOldAreYou {
 			// readLine() は、入出力エラーの可能性がある。エラー処理がないとコンパイルできない。
 			//  Java では、 try{ XXXXXXXX }  catch(エラーの型 変数) { XXXXXXXXXXXXXXXXXX} と書く
 		try {
+			while(true) {
 			System.out.println("何歳ですか?");
 			String line = reader.readLine();
+			if(line.equals("q")||line.equals("e")){
+            break;
+			}
 			int age = Integer.parseInt(line);
 			System.out.println("あなたは" + age + "歳ですね。");
 			System.out.println("あなたは2030年の時、" + (age + 2030-2024) + "歳ですね。");
             if(0<=age&&age<=6) {
-                System.out.println("あなたは令和"+(6-age)+"年生まれですね。");
+                System.out.println("あなたは令和"+(7-age)+"年生まれですね。");
             }else if(7<=age&&age<=35) {
-                System.out.println("あなたは平成"+(35-age)+"年生まれですね。");
+                System.out.println("あなたは平成"+(36-age)+"年生まれですね。");
             }else if(36<=age&&age<=98) {
-                System.out.println("あなたは昭和"+(98-age)+"年生まれですね。");
-            }else if(99<=age&&age<=120){
-                System.out.println("あなたは大正"+(120-age)+"年生まれですね。");
-            }
+                System.out.println("あなたは昭和"+(99-age)+"年生まれですね。");
+            }else if(99<=age&&age<=112){
+                System.out.println("あなたは大正"+(113-age)+"年生まれですね。");
+            }else if(113<=age&&age<120){
+                System.out.println("あなたは明治"+(121-age)+"年生まれですね。");
+            }else{
+				System.out.println("再入力してください。");
+			}
+		}
 		}
 		catch(IOException e) {
 			System.out.println(e);
